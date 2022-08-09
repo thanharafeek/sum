da3f17374fc2d2740c1a811a4962db92100837c7
thanharafeek <110898208+thanharafeek@users.noreply.github.com>
2022-08-09 15:20:04 +0530

    Add files via upload

---
+++ nestedkey.py
@@ -0,0 +1,24 @@
+nesteddictionery={
+        "one":
+        {
+        "name":"john",
+        "age":30,
+        "city":"NEW YORK"
+},
+    "two":
+{
+    "name":"jane",
+    "age":25,
+    "city":"PARIS"
+},
+    "three":
+{
+    "name":"bobby",
+    "age":20,
+    "city":"LONDON"
+}
+}
+print(nesteddictionery)
+print(nesteddictionery["three"]["name"])
+
+
